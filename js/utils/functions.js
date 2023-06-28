d60c98c2d5147086df362496edf102e1d77734d5
MartinezFabian <FabianMartinezDev@gmail.com>
2023-06-28 20:19:29 -0300

    feat: Add functionality to fetch all the current trading info

--- js/utils/functions.js
+++ js/utils/functions.js
@@ -43,7 +43,32 @@ function validateSelections(e) {
     return;
   }
 
-  console.log(selectionsObject);
+  // si ambos campos fueron seleccionados
+  fetchAllTheCurrentTradingInfo();
+}
+
+async function fetchAllTheCurrentTradingInfo() {
+  const { cryptocurrency, currency } = selectionsObject;
+
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptocurrency}&tsyms=${currency}`;
+
+  try {
+    // realizar la solicitud GET a la API
+    const response = await fetch(url);
+
+    // si la respuesta es exitosa
+    if (response.ok) {
+      // convertir la respuesta en un objeto JavaScript
+      const data = await response.json();
+
+      console.log(data.DISPLAY[cryptocurrency][currency]);
+    } else {
+      // si la respuesta no es exitosa lanzar un error
+      throw new Error(`Error: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export { fetchCryptocurrencies, getSelection, validateSelections };
