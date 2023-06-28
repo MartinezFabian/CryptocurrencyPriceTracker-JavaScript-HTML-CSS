6272b80f2e9eed63d196900ddc2bad16b87f66c2
MartinezFabian <FabianMartinezDev@gmail.com>
2023-06-27 21:41:07 -0300

    feat: Add functionality to add cryptocurrencies to the select

--- js/classes/UserInterface.js
+++ js/classes/UserInterface.js
@@ -1,6 +1,19 @@
+import { selectCryptocurrency } from "../utils/selectors.js";
+
 class UserInterface {
   static AddCryptocurrenciesToSelect(cryptocurrencies) {
-    console.log(cryptocurrencies);
+    cryptocurrencies.forEach((crypto) => {
+      const {
+        CoinInfo: { FullName, Name },
+      } = crypto;
+
+      // crear los <option></option>
+      const option = document.createElement("option");
+      option.value = Name;
+      option.textContent = FullName;
+
+      selectCryptocurrency.appendChild(option);
+    });
   }
 }
 
